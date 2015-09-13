d2b79ec3c64169e87b79729fce85d5d8a2ec1b6c
astrapi69 <asterios.raptis@gmx.net>
2015-09-13 10:41:40 +0200

    created .travis.yml file and fixed unit tests.

--- c10n.interface.generator/src/test/java/de/alpharogroup/c10n/iface/generator/C10NInterfaceGeneratorTest.java
+++ c10n.interface.generator/src/test/java/de/alpharogroup/c10n/iface/generator/C10NInterfaceGeneratorTest.java
@@ -9,33 +9,34 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 
-import net.sourceforge.jaulp.collections.SortedProperties;
-import net.sourceforge.jaulp.file.search.PathFinder;
-
 import org.testng.annotations.Test;
 
 import c10n.C10N;
 import c10n.C10NConfigBase;
 import c10n.annotations.DefaultC10NAnnotations;
 import de.alpharogroup.c10n.iface.generator.util.PropertiesValidation;
+import de.alpharogroup.collections.SortedProperties;
+import de.alpharogroup.file.search.PathFinder;
 
 
-public class C10NInterfaceGeneratorTest {
+public class C10NInterfaceGeneratorTest
+{
 
-	@Test(enabled=true)
-	public void testGenerate() throws IOException {
-		Map<String, Properties> propertiesWithAnnotationNames = new LinkedHashMap<String, Properties>();
-		Properties enProperties = new SortedProperties();
+	@Test(enabled = true)
+	public void testGenerate() throws IOException
+	{
+		final Map<String, Properties> propertiesWithAnnotationNames = new LinkedHashMap<String, Properties>();
+		final Properties enProperties = new SortedProperties();
 
 		enProperties.put("com", "Hello, {0} {1} {2}!");
 		enProperties.put("com.example.gui.window.title", "Hello, {0}!");
 		enProperties.put("com.example.gui.window.buttons.ok", "OK");
 		enProperties.put("com.example.gui.window.buttons.cancel", "Cancel");
-		
+
 		String localeAnnotationName = "En";
 		propertiesWithAnnotationNames.put(localeAnnotationName, enProperties);
-		
-		Properties ruProperties = new SortedProperties();
+
+		final Properties ruProperties = new SortedProperties();
 
 		ruProperties.put("com", "Привет, {0} {1} {2}!");
 		ruProperties.put("com.example.gui.window.title", "Привет, {0}!");
@@ -44,57 +45,62 @@ public class C10NInterfaceGeneratorTest {
 
 		localeAnnotationName = "Ru";
 		propertiesWithAnnotationNames.put(localeAnnotationName, ruProperties);
-		
-		C10NInterfaceGenerator generator = new C10NInterfaceGenerator(propertiesWithAnnotationNames, "src/main/java/");
-		generator.generate();	
+
+		final C10NInterfaceGenerator generator = new C10NInterfaceGenerator(
+			propertiesWithAnnotationNames, "src/main/java/");
+		generator.generate();
 
 	}
 
-	@Test(enabled=false)
-	public void testProperties() throws IOException {
-		File rootDir = PathFinder.getSrcTestResourcesDir();
-		
-		Map<String, List<Properties>> propertiesWithAnnotationNames = PropertiesValidation
-				.getMapForC10NInterfaceGeneration(rootDir);
-		for(Entry<String, List<Properties>> entry : propertiesWithAnnotationNames.entrySet()){
-			String key = entry.getKey();
-			List<Properties> propertiesList = entry.getValue();
-			for(Properties p : propertiesList){
-				Map<String, Properties> propWithAnnotationNames = new LinkedHashMap<String, Properties>();
-				propWithAnnotationNames.put(key, p);
-				C10NInterfaceGenerator generator = new
-				 C10NInterfaceGenerator(propWithAnnotationNames,
-				 "src/main/generated/");
-				 generator.generate();
+	public void testMessage()
+	{
+
+		C10N.configure(new C10NConfigBase()
+		{
+			@Override
+			public void configure()
+			{
+				install(new DefaultC10NAnnotations());
+
 			}
-		}		 
+		});
+		// uncomment if intefaces are generated...
+		// Window msg = C10N.get(Window.class, Locale.ENGLISH);
+		// String result = msg.title("bla");
+		// System.out.println(result);
+		//
+		// Buttons buttons = C10N.get(Buttons.class, Locale.ENGLISH);
+		// result = buttons.cancel();
+		// System.out.println(result);
+		//
+		// buttons = C10N.get(Buttons.class, new Locale("ru"));
+		//
+		// result = buttons.cancel();
+		// System.out.println(result);
+
+
 	}
 
-	public void testMessage(){
+	@Test(enabled = false)
+	public void testProperties() throws IOException
+	{
+		final File rootDir = PathFinder.getSrcTestResourcesDir();
 
-		C10N.configure(new C10NConfigBase(){
-		    @Override
-		    public void configure() {
-		      install(new DefaultC10NAnnotations());
-
-		    }
-		  });
-		// uncomment if intefaces are generated...
-//		Window msg = C10N.get(Window.class, Locale.ENGLISH);
-//		String result = msg.title("bla");
-//		System.out.println(result);
-//		
-//		Buttons buttons = C10N.get(Buttons.class, Locale.ENGLISH);
-//		result = buttons.cancel();
-//		System.out.println(result);
-//		
-//		buttons = C10N.get(Buttons.class, new Locale("ru"));
-//		
-//		result = buttons.cancel();
-//		System.out.println(result);
-
-
+		final Map<String, List<Properties>> propertiesWithAnnotationNames = PropertiesValidation
+			.getMapForC10NInterfaceGeneration(rootDir);
+		for (final Entry<String, List<Properties>> entry : propertiesWithAnnotationNames.entrySet())
+		{
+			final String key = entry.getKey();
+			final List<Properties> propertiesList = entry.getValue();
+			for (final Properties p : propertiesList)
+			{
+				final Map<String, Properties> propWithAnnotationNames = new LinkedHashMap<String, Properties>();
+				propWithAnnotationNames.put(key, p);
+				final C10NInterfaceGenerator generator = new C10NInterfaceGenerator(
+					propWithAnnotationNames, "src/main/generated/");
+				generator.generate();
+			}
+		}
 	}
 
 }
-
