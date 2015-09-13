2459cc78cd5bff64e16fe2100bd2c066d7970b43
astrapi69 <asterios.raptis@gmx.net>
2015-09-13 10:22:39 +0200

    changed to jdk 1.8

--- c10n.interface.generator/src/main/java/de/alpharogroup/c10n/iface/generator/C10NInterfaceGenerator.java
+++ c10n.interface.generator/src/main/java/de/alpharogroup/c10n/iface/generator/C10NInterfaceGenerator.java
@@ -27,92 +27,109 @@ import c10n.annotations.Zh;
 
 import com.google.common.io.Files;
 
-import net.sourceforge.jaulp.file.FileExtension;
-import net.sourceforge.jaulp.lang.PropertiesUtils;
-import net.sourceforge.jaulp.lang.model.AnnotationModel;
-import net.sourceforge.jaulp.lang.model.ClassModel;
-import net.sourceforge.jaulp.lang.model.MethodModel;
-import net.sourceforge.jaulp.locale.Locales;
-import net.sourceforge.jaulp.string.StringUtils;
+import de.alpharogroup.file.FileExtension;
+import de.alpharogroup.lang.PropertiesUtils;
+import de.alpharogroup.lang.model.AnnotationModel;
+import de.alpharogroup.lang.model.ClassModel;
+import de.alpharogroup.lang.model.MethodModel;
+import de.alpharogroup.locale.Locales;
+import de.alpharogroup.string.StringUtils;
 
-public class C10NInterfaceGenerator {
+public class C10NInterfaceGenerator
+{
 	// Holds the data for the inteface creation.
 	Map<String, ClassModel> interfaces = new LinkedHashMap<String, ClassModel>();
-	
+
 	// Holds the AnnotationName for the locale and the corresponding properties object.
 	Map<String, Properties> propertiesWithAnnotationNames;
 
 	@SuppressWarnings("serial")
-	Map<String, Class<?>> stringToAnnotationClass = new HashMap<String, Class<?>>(){{
-		put("En", En.class);
-		put("De", De.class);
-		put("Fr", Fr.class);
-		put("It", It.class);
-		put("Ja", Ja.class);
-		put("Ko", Ko.class);
-		put("Ru", Ru.class);
-		put("Zh", Zh.class);
-	}};
+	Map<String, Class<?>> stringToAnnotationClass = new HashMap<String, Class<?>>()
+	{
+		{
+			put("En", En.class);
+			put("De", De.class);
+			put("Fr", Fr.class);
+			put("It", It.class);
+			put("Ja", Ja.class);
+			put("Ko", Ko.class);
+			put("Ru", Ru.class);
+			put("Zh", Zh.class);
+		}
+	};
 	@SuppressWarnings("serial")
-	Map<Class<?>, Locale> annotationToLocale = new HashMap<Class<?>, Locale>(){{
-		put(En.class, Locale.ENGLISH);
-		put(De.class, Locale.GERMAN);
-		put(Fr.class, Locale.FRENCH);
-		put(It.class, Locale.ITALIAN);
-		put(Ja.class, Locale.JAPANESE);
-		put(Ko.class, Locale.KOREAN);
-		put(Ru.class, Locales.RUSSIAN);
-		put(Zh.class, Locale.CHINESE);
-	}};
+	Map<Class<?>, Locale> annotationToLocale = new HashMap<Class<?>, Locale>()
+	{
+		{
+			put(En.class, Locale.ENGLISH);
+			put(De.class, Locale.GERMAN);
+			put(Fr.class, Locale.FRENCH);
+			put(It.class, Locale.ITALIAN);
+			put(Ja.class, Locale.JAPANESE);
+			put(Ko.class, Locale.KOREAN);
+			put(Ru.class, Locales.RUSSIAN);
+			put(Zh.class, Locale.CHINESE);
+		}
+	};
 	@SuppressWarnings("serial")
-	Map<Locale, Class<?>> localeToannotation = new HashMap<Locale, Class<?>>(){{
-		put(Locale.ENGLISH, En.class);
-		put(Locale.GERMAN, De.class);
-		put(Locale.FRENCH, Fr.class);
-		put(Locale.ITALIAN, It.class);
-		put(Locale.JAPANESE, Ja.class);
-		put(Locale.KOREAN, Ko.class);
-		put(Locales.RUSSIAN, Ru.class);
-		put(Locale.CHINESE, Zh.class);
-	}};
+	Map<Locale, Class<?>> localeToannotation = new HashMap<Locale, Class<?>>()
+	{
+		{
+			put(Locale.ENGLISH, En.class);
+			put(Locale.GERMAN, De.class);
+			put(Locale.FRENCH, Fr.class);
+			put(Locale.ITALIAN, It.class);
+			put(Locale.JAPANESE, Ja.class);
+			put(Locale.KOREAN, Ko.class);
+			put(Locales.RUSSIAN, Ru.class);
+			put(Locale.CHINESE, Zh.class);
+		}
+	};
 
 	String sourceFolderForInterfaces;
 
-	public C10NInterfaceGenerator(
-			Map<String, Properties> propertiesWithAnnotationNames,
-			String sourceFolderForInterfaces) {
-		if(propertiesWithAnnotationNames == null || propertiesWithAnnotationNames.isEmpty()){
-			throw new IllegalArgumentException("propertiesWithAnnotationNames should not be null or empty.");
+	public C10NInterfaceGenerator(final Map<String, Properties> propertiesWithAnnotationNames,
+		final String sourceFolderForInterfaces)
+	{
+		if ((propertiesWithAnnotationNames == null) || propertiesWithAnnotationNames.isEmpty())
+		{
+			throw new IllegalArgumentException(
+				"propertiesWithAnnotationNames should not be null or empty.");
 		}
-		if(sourceFolderForInterfaces == null || sourceFolderForInterfaces.isEmpty()){
-			throw new IllegalArgumentException("sourceFolderForInterfaces should not be null or empty.");
+		if ((sourceFolderForInterfaces == null) || sourceFolderForInterfaces.isEmpty())
+		{
+			throw new IllegalArgumentException(
+				"sourceFolderForInterfaces should not be null or empty.");
 		}
 		this.propertiesWithAnnotationNames = propertiesWithAnnotationNames;
 		this.sourceFolderForInterfaces = sourceFolderForInterfaces;
 		init();
 	}
-	public void generate() throws IOException{
-		for (Map.Entry<String, ClassModel> entry : interfaces.entrySet()) {
-			ClassModel classModel = entry.getValue();
 
-			VelocityContext context = new VelocityContext();
+	public void generate() throws IOException
+	{
+		for (final Map.Entry<String, ClassModel> entry : interfaces.entrySet())
+		{
+			final ClassModel classModel = entry.getValue();
+
+			final VelocityContext context = new VelocityContext();
 			context.put("model", classModel);
-			Template daoClassTemplate = Velocity
-					.getTemplate("src/main/resources/c10nInterface.vm");
+			final Template daoClassTemplate = Velocity
+				.getTemplate("src/main/resources/c10nInterface.vm");
 			System.out.println(classModel);
 
-			String packageAndClassName = classModel.getClassName().equalsIgnoreCase(classModel.getPackageName())
-					? classModel.getPackageName()+"/"+classModel.getClassName()
-							:classModel.getPackageName().replace('.', '/')+"/"
-							+ classModel.getClassName();
-			File classFile = new File(sourceFolderForInterfaces
-					+ packageAndClassName + FileExtension.JAVA.getExtension());
+			final String packageAndClassName = classModel.getClassName().equalsIgnoreCase(
+				classModel.getPackageName()) ? classModel.getPackageName() + "/"
+				+ classModel.getClassName() : classModel.getPackageName().replace('.', '/') + "/"
+				+ classModel.getClassName();
+			final File classFile = new File(sourceFolderForInterfaces + packageAndClassName
+				+ FileExtension.JAVA.getExtension());
 			Files.createParentDirs(classFile);
-			if (!classFile.exists()) {				
+			if (!classFile.exists())
+			{
 				classFile.createNewFile();
 			}
-			BufferedWriter writer = new BufferedWriter(
-					new FileWriter(classFile));
+			final BufferedWriter writer = new BufferedWriter(new FileWriter(classFile));
 
 			daoClassTemplate.merge(context, writer);
 			writer.flush();
@@ -120,89 +137,105 @@ public class C10NInterfaceGenerator {
 
 		}
 	}
-	private void getClassModels(Map<String, List<String>> matchedPrefixes,
-			String localeAnnotationName, Map<String, ClassModel> interfaces,
-			Properties ruProperties) {
-		for (Map.Entry<String, List<String>> entry : matchedPrefixes.entrySet()) {
-			String key = entry.getKey();
-			List<String> propertyKeyValues = entry.getValue();
+
+	private void getClassModels(final Map<String, List<String>> matchedPrefixes,
+		final String localeAnnotationName, final Map<String, ClassModel> interfaces,
+		final Properties ruProperties)
+	{
+		for (final Map.Entry<String, List<String>> entry : matchedPrefixes.entrySet())
+		{
+			final String key = entry.getKey();
+			final List<String> propertyKeyValues = entry.getValue();
 			ClassModel classModel = null;
 			Map<String, MethodModel> methods = null;
-			int lastIndex = key.lastIndexOf(".");
+			final int lastIndex = key.lastIndexOf(".");
 			String packageName = "";
 			String interfaceName = null;
 			String methodName = "";
 			MethodModel method = null;
-			if (0 < lastIndex) {
-				interfaceName =StringUtils.setFirstCharacterToUpperCase( key.substring(lastIndex + 1, key.length()));
-				if (interfaces.containsKey(interfaceName)) {
+			if (0 < lastIndex)
+			{
+				interfaceName = StringUtils.setFirstCharacterToUpperCase(key.substring(
+					lastIndex + 1, key.length()));
+				if (interfaces.containsKey(interfaceName))
+				{
 					classModel = interfaces.get(interfaceName);
 					methods = classModel.getMethods();
-					for (String value : propertyKeyValues) {
-						methodName = value.substring(key.length() + 1,
-								value.length());
-						
+					for (final String value : propertyKeyValues)
+					{
+						methodName = value.substring(key.length() + 1, value.length());
+
 						method = methods.get(methodName);
-						if(method != null){
-							String propertyValue = (String) ruProperties.get(value);
-							AnnotationModel annotation = new AnnotationModel();
+						if (method != null)
+						{
+							final String propertyValue = (String)ruProperties.get(value);
+							final AnnotationModel annotation = new AnnotationModel();
 							method.getMethodAnnotations().add(annotation);
 							annotation.setName(localeAnnotationName);
 							annotation.setValue(propertyValue);
 							method.setParameters(PropertiesUtils
-									.getPropertyParameters(propertyValue));							
-						} else {
+								.getPropertyParameters(propertyValue));
+						}
+						else
+						{
 							System.err.println("Method does not exists in classmodel...\n"
-									+ "classname:"+classModel.getClassName()+"\n"
-											+ "packageName:"+classModel.getPackageName());
+								+ "classname:" + classModel.getClassName() + "\n" + "packageName:"
+								+ classModel.getPackageName());
 						}
 					}
-				} else {
+				}
+				else
+				{
 					classModel = new ClassModel();
 					classModel.setClassName(interfaceName);
 					methods = new LinkedHashMap<String, MethodModel>();
 					classModel.setMethods(methods);
 					packageName = key.substring(0, lastIndex);
 					classModel.setPackageName(packageName);
-					for (String value : propertyKeyValues) {
-						methodName = value.substring(key.length() + 1,
-								value.length());
+					for (final String value : propertyKeyValues)
+					{
+						methodName = value.substring(key.length() + 1, value.length());
 						method = new MethodModel();
 						method.setMethodAnnotations(new ArrayList<AnnotationModel>());
 						methods.put(methodName, method);
 						method.setMethodName(methodName);
-						String propertyValue = (String) ruProperties.get(value);
-						AnnotationModel annotation = new AnnotationModel();
+						final String propertyValue = (String)ruProperties.get(value);
+						final AnnotationModel annotation = new AnnotationModel();
 						method.getMethodAnnotations().add(annotation);
 						annotation.setName(localeAnnotationName);
 						annotation.setValue(propertyValue);
-						method.setParameters(PropertiesUtils
-								.getPropertyParameters(propertyValue));
+						method.setParameters(PropertiesUtils.getPropertyParameters(propertyValue));
 					}
-					String cn = StringUtils.setFirstCharacterToUpperCase(classModel.getClassName());
+					final String cn = StringUtils.setFirstCharacterToUpperCase(classModel
+						.getClassName());
 					classModel.setClassName(cn);
 					interfaces.put(interfaceName, classModel);
 				}
 
-			} else {
+			}
+			else
+			{
 				interfaceName = key;
-				if (interfaces.containsKey(interfaceName)) {
+				if (interfaces.containsKey(interfaceName))
+				{
 					classModel = interfaces.get(interfaceName);
 					packageName = interfaceName.toLowerCase();
 					classModel.setPackageName(packageName);
 					methods = classModel.getMethods();
-					for (String value : propertyKeyValues) {
+					for (final String value : propertyKeyValues)
+					{
 						methodName = value;
 						method = methods.get(methodName);
-						String propertyValue = (String) ruProperties.get(value);
-						AnnotationModel annotation = new AnnotationModel();
+						final String propertyValue = (String)ruProperties.get(value);
+						final AnnotationModel annotation = new AnnotationModel();
 						method.getMethodAnnotations().add(annotation);
 						annotation.setName(localeAnnotationName);
 						annotation.setValue(propertyValue);
-						method.setParameters(PropertiesUtils
-								.getPropertyParameters(propertyValue));
+						method.setParameters(PropertiesUtils.getPropertyParameters(propertyValue));
 					}
-				} else {
+				}
+				else
+				{
 					interfaceName = key;
 					classModel = new ClassModel();
 					packageName = interfaceName.toLowerCase();
@@ -210,21 +243,22 @@ public class C10NInterfaceGenerator {
 					methods = new LinkedHashMap<String, MethodModel>();
 					classModel.setMethods(methods);
 					classModel.setClassName(interfaceName);
-					for (String value : propertyKeyValues) {
+					for (final String value : propertyKeyValues)
+					{
 						methodName = value;
 						method = new MethodModel();
 						method.setMethodAnnotations(new ArrayList<AnnotationModel>());
 						methods.put(methodName, method);
 						method.setMethodName(methodName);
-						String propertyValue = (String) ruProperties.get(value);
-						AnnotationModel annotation = new AnnotationModel();
+						final String propertyValue = (String)ruProperties.get(value);
+						final AnnotationModel annotation = new AnnotationModel();
 						method.getMethodAnnotations().add(annotation);
 						annotation.setName(localeAnnotationName);
 						annotation.setValue(propertyValue);
-						method.setParameters(PropertiesUtils
-								.getPropertyParameters(propertyValue));
+						method.setParameters(PropertiesUtils.getPropertyParameters(propertyValue));
 					}
-					String cn = StringUtils.setFirstCharacterToUpperCase(classModel.getClassName());
+					final String cn = StringUtils.setFirstCharacterToUpperCase(classModel
+						.getClassName());
 					classModel.setClassName(cn);
 					interfaces.put(interfaceName, classModel);
 				}
@@ -233,15 +267,16 @@ public class C10NInterfaceGenerator {
 	}
 
 
-	private void init() {
+	private void init()
+	{
 		Map<String, List<String>> matchedPrefixes;
-		for (Map.Entry<String, Properties> entry : this.propertiesWithAnnotationNames.entrySet()) {
-			String key = entry.getKey();
-			Properties value = entry.getValue();
-			matchedPrefixes = PropertiesUtils
-					.getMatchedPrefixLists(value);
-			getClassModels(matchedPrefixes, key, interfaces,
-					value);
-		}		
+		for (final Map.Entry<String, Properties> entry : this.propertiesWithAnnotationNames
+			.entrySet())
+		{
+			final String key = entry.getKey();
+			final Properties value = entry.getValue();
+			matchedPrefixes = PropertiesUtils.getMatchedPrefixLists(value);
+			getClassModels(matchedPrefixes, key, interfaces, value);
+		}
 	}
 }
--- c10n.interface.generator/src/main/java/de/alpharogroup/c10n/iface/generator/util/PropertiesValidation.java
+++ c10n.interface.generator/src/main/java/de/alpharogroup/c10n/iface/generator/util/PropertiesValidation.java
@@ -19,102 +19,99 @@ import c10n.annotations.Ja;
 import c10n.annotations.Ko;
 import c10n.annotations.Ru;
 import c10n.annotations.Zh;
-import net.sourceforge.jaulp.file.search.FileSearchUtils;
-import net.sourceforge.jaulp.lang.PropertiesUtils;
-import net.sourceforge.jaulp.locale.Locales;
+import de.alpharogroup.file.search.FileSearchUtils;
+import de.alpharogroup.lang.PropertiesUtils;
+import de.alpharogroup.locale.Locales;
 
-public class PropertiesValidation {
+public class PropertiesValidation
+{
 
 	@SuppressWarnings("serial")
-	public static final Map<Locale, Class<?>> localeToannotation = new HashMap<Locale, Class<?>>(){{
-		put(Locale.ENGLISH, En.class);
-		put(Locale.GERMAN, De.class);
-		put(Locale.FRENCH, Fr.class);
-		put(Locale.ITALIAN, It.class);
-		put(Locale.JAPANESE, Ja.class);
-		put(Locale.KOREAN, Ko.class);
-		put(Locales.RUSSIAN, Ru.class);
-		put(Locale.CHINESE, Zh.class);
-	}};
-	public static Collection<File> validate(File rootDir) throws IOException{
-		// 1. Replace all invalid characters.
-		// Collection<File> propertiesFiles = PropertiesNormalizer.findPropertiesFilesWithInvalidCharacters(rootDir);
-		// 2. Find duplicate keys.
-//		for (File propertiesFile : propertiesFiles) {
-//			KeyValueLists keyValueLists = DuplicatePropertiesKeyFinder.findDuplicateKeys(propertiesFile);
-//			// TODO implement the algorithm for it...
-//		}
-//		// 3. Find duplicate values.
-//		for (File propertiesFile : propertiesFiles) {
-//			Properties properties = PropertiesUtils.loadProperties(propertiesFile);
-//			Map<String, List<String>> redundantValues =PropertiesUtils.findRedundantValues(properties);
-//			// TODO implement the algorithm for it...
-//		}
-//		return propertiesFiles;	
-		return null;		
-	}
-	
-	public static Map<Properties, Locale> mapWithLocales(Collection<File> propertiesFiles) throws IOException{
-		Map<Properties, Locale> propertiesToLocales = new HashMap<>();
-		for (File propertiesFile : propertiesFiles) {
-			Properties properties = PropertiesUtils.loadProperties(propertiesFile);
-			Locale locale = PropertiesUtils.getLocale(propertiesFile);
-			propertiesToLocales.put(properties, locale);			
+	public static final Map<Locale, Class<?>> localeToannotation = new HashMap<Locale, Class<?>>()
+	{
+		{
+			put(Locale.ENGLISH, En.class);
+			put(Locale.GERMAN, De.class);
+			put(Locale.FRENCH, Fr.class);
+			put(Locale.ITALIAN, It.class);
+			put(Locale.JAPANESE, Ja.class);
+			put(Locale.KOREAN, Ko.class);
+			put(Locales.RUSSIAN, Ru.class);
+			put(Locale.CHINESE, Zh.class);
 		}
-		return propertiesToLocales;
+	};
+
+	public static List<File> findFiles(final File rootDir)
+	{
+		final String pattern = "*.properties";
+		final List<File> foundedFiles = FileSearchUtils.findFilesRecursive(rootDir, pattern);
+		return foundedFiles;
 	}
-	
-	public static Map<String, List<Properties>> getMapForC10NInterfaceGeneration(Collection<File> propertiesFiles) throws IOException{
-		Map<String, List<Properties>> propertiesWithAnnotationNames = new LinkedHashMap<String, List<Properties>>();
-		for (File propertiesFile : propertiesFiles) {
-			Properties properties = PropertiesUtils.loadProperties(propertiesFile);
-			Locale locale = PropertiesUtils.getLocale(propertiesFile);
-			if(localeToannotation.containsKey(locale)){
-				Class<?> c10nAnnotation = localeToannotation.get(locale);
-				if(!propertiesWithAnnotationNames.containsKey(c10nAnnotation.getSimpleName())){
-						propertiesWithAnnotationNames.put(c10nAnnotation.getSimpleName(), new ArrayList<Properties>());
-					
+
+	public static Map<String, List<Properties>> getMapForC10NInterfaceGeneration(
+		final Collection<File> propertiesFiles) throws IOException
+	{
+		final Map<String, List<Properties>> propertiesWithAnnotationNames = new LinkedHashMap<String, List<Properties>>();
+		for (final File propertiesFile : propertiesFiles)
+		{
+			final Properties properties = PropertiesUtils.loadProperties(propertiesFile);
+			final Locale locale = PropertiesUtils.getLocale(propertiesFile);
+			if (localeToannotation.containsKey(locale))
+			{
+				final Class<?> c10nAnnotation = localeToannotation.get(locale);
+				if (!propertiesWithAnnotationNames.containsKey(c10nAnnotation.getSimpleName()))
+				{
+					propertiesWithAnnotationNames.put(c10nAnnotation.getSimpleName(),
+						new ArrayList<Properties>());
+
 				}
 				propertiesWithAnnotationNames.get(c10nAnnotation.getSimpleName()).add(properties);
 			}
 		}
 		return propertiesWithAnnotationNames;
 	}
-	
-	public static Map<String, List<Properties>> getMapForC10NInterfaceGeneration(File rootDir) throws IOException{
-		Collection<File> propertiesFiles = findFiles(rootDir);
+
+	public static Map<String, List<Properties>> getMapForC10NInterfaceGeneration(final File rootDir)
+		throws IOException
+	{
+		final Collection<File> propertiesFiles = findFiles(rootDir);
 		return getMapForC10NInterfaceGeneration(propertiesFiles);
 	}
-	
+
+	public static Map<Properties, Locale> mapWithLocales(final Collection<File> propertiesFiles)
+		throws IOException
+	{
+		final Map<Properties, Locale> propertiesToLocales = new HashMap<>();
+		for (final File propertiesFile : propertiesFiles)
+		{
+			final Properties properties = PropertiesUtils.loadProperties(propertiesFile);
+			final Locale locale = PropertiesUtils.getLocale(propertiesFile);
+			propertiesToLocales.put(properties, locale);
+		}
+		return propertiesToLocales;
+	}
 
 
-	/**
-	 * Finds all properties files and adds them to the
-	 * collection from the given directory recursively.
-	 * 
-	 * @param rootDir
-	 *            the root directory that shall be search for all properties files.
-	 * @return a collection with all found properties files.
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-//	public static Collection<File> findAllPropertiesFiles(File rootDir) throws IOException {
-//		final Collection<File> found = new ArrayList<File>();
-//		PropertiesDirectoryWalker walker = new PropertiesDirectoryWalker() {
-//			@Override
-//			protected void handleFile(File file, int depth,
-//					Collection<File> results) throws IOException {				
-//					found.add(file);				
-//			}
-//		};
-//		walker.start(rootDir);
-//		return found;
-//	}
-	
-	public static List<File> findFiles(File rootDir) {
-		String pattern = "*.properties";
-		List<File> foundedFiles = FileSearchUtils.findFilesRecursive(rootDir, pattern);
-		return foundedFiles;
+	public static Collection<File> validate(final File rootDir) throws IOException
+	{
+		// 1. Replace all invalid characters.
+		// Collection<File> propertiesFiles =
+		// PropertiesNormalizer.findPropertiesFilesWithInvalidCharacters(rootDir);
+		// 2. Find duplicate keys.
+		// for (File propertiesFile : propertiesFiles) {
+		// KeyValueLists keyValueLists =
+		// DuplicatePropertiesKeyFinder.findDuplicateKeys(propertiesFile);
+		// // TODO implement the algorithm for it...
+		// }
+		// // 3. Find duplicate values.
+		// for (File propertiesFile : propertiesFiles) {
+		// Properties properties = PropertiesUtils.loadProperties(propertiesFile);
+		// Map<String, List<String>> redundantValues
+		// =PropertiesUtils.findRedundantValues(properties);
+		// // TODO implement the algorithm for it...
+		// }
+		// return propertiesFiles;
+		return null;
 	}
 
 }
